54741cf7da6aa17f879a74d2d2849c04d66c54a2
Muhammad khizer <khizer.younus@simpaisa.com>
2023-01-14 00:18:32 +0500

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 
-const port = process.env.PORt || 3001;
+const port = process.env.PORT || 3001;
 const indexRoute = require("./routes/index");
 const StockRoute = require("./routes/stock");
 const ReviewRoute = require("./routes/review");
