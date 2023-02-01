2d0019640347cd06b65a37ed0d6b2d2d7112fe64
Muhammad khizer <khizer.younus@simpaisa.com>
2023-02-01 17:02:37 +0500

    yahoo finance 2

--- routes/stock.js
+++ routes/stock.js
@@ -1,24 +1,48 @@
 const express = require("express");
 const router = express.Router();
-var yahooFinance = require('yahoo-finance');
+var yahooFinance = require("yahoo-finance");
+const yahooFinance2 = require("yahoo-finance2").default;
 
 router.post("/", async (req, res) => {
   let data = null;
   let symbol = req.body.symbol;
 
-  try{
-    if(symbol){
-        yahooFinance.quote({
+  try {
+    if (symbol) {
+      yahooFinance.quote(
+        {
           symbol: symbol,
-          modules: [  'financialData' , 'summaryDetail' , 'price' , 'summaryProfile' ] // see the docs for the full list
-        }, function (err, quotes) {
+          modules: [
+            "financialData",
+            "summaryDetail",
+            "price",
+            "summaryProfile",
+          ], // see the docs for the full list
+        },
+        function (err, quotes) {
           data = quotes;
           res.send({ response: data }).status(200);
           // res.send({ response: "My name is dash" }).status(200);
-        });
+        }
+      );
     }
+  } catch (err) {
+    res.send({ response: data }).status(200);
+  }
+});
 
-  }catch(err){
+router.post("/yahoo", async (req, res) => {
+  let data = null;
+  let symbol = req.body.symbol;
+
+  try {
+    if (symbol) {
+      const quote = await yahooFinance2.quoteSummary(symbol, {
+        modules: ["financialData", "balanceSheetHistoryQuarterly", "price" , "assetProfile"],
+      });
+      res.send({ response: quote }).status(200);
+    }
+  } catch (err) {
     res.send({ response: data }).status(200);
   }
 });
