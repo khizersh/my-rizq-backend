fda2c74b98d34b53c715444f82d967e906d29ff3
khizersh <moh.khizer61@gmail.com>
2023-04-03 20:14:43 +0500

    live

--- routes/stripe.js
+++ routes/stripe.js
@@ -4,10 +4,10 @@ const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const Fs = require("fs");
 const CsvReadableStream = require("csv-reader");
 const Stripe = require('stripe');
-const stripe = Stripe("sk_test_51Kmn42HxzdoZduY7tcd4RuB7hL9pwQY9r6FQyDOw0zs8oGvhM8M4yEzXEypUdx39bl3KzR72Ejcd4wS3GKmgoJyc00gXk8WQWL");
+// const stripe = Stripe("sk_test_51Kmn42HxzdoZduY7tcd4RuB7hL9pwQY9r6FQyDOw0zs8oGvhM8M4yEzXEypUdx39bl3KzR72Ejcd4wS3GKmgoJyc00gXk8WQWL");
+const stripe = Stripe("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");
 
 router.post("/charge", async (req, res) => {
-    console.log("stripe-routes.js 9 | route reached", req.body);
     let { amount, id } = req.body;
     console.log("stripe-routes.js 10 | amount and id", amount, id);
     try {
@@ -18,15 +18,15 @@ router.post("/charge", async (req, res) => {
         payment_method: id,
         confirm: true,
       });
-      console.log("stripe-routes.js 19 | payment", payment);
+      console.log("payment : ",payment);
       res.json({
         message: "Payment Successful",
         success: true,
       });
     } catch (error) {
-      console.log("stripe-routes.js 17 | error", error);
+      console.log("stripe-routes.js 17 | error", error?.raw?.message);
       res.json({
-        message: "Payment Failed",
+        message: error?.raw?.message,
         success: false,
       });
     }
